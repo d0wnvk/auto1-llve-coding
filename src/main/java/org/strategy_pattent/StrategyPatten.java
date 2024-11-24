123e6b61885b615be5e9f5eba88a865599908a67
Vladimir_Korolevskii <vladimir_korolevskii@epam.com>
2024-11-24 16:44:08 +0100

    if-switch to polymorphism

---
+++ src/main/java/org/strategy_pattent/StrategyPatten.java
@@ -0,0 +1,86 @@
+package org.strategy_pattent;
+
+import java.util.Map;
+
+public class StrategyPatten {
+
+    /**
+     * Let's have a classic if- and switch-related solutions.
+     * Not covered with tests.
+     * */
+    void ifAndSwitchCase(String text) {
+        if (text.equals("A")) {
+            System.out.println("if A");
+        }
+
+        if (text.equals("B")) {
+            System.out.println("if B");
+        }
+
+        switch (text) {
+            case "A":
+                System.out.println("switch A");
+                break;
+            case "B":
+                System.out.println("switch b");
+                break;
+            default:
+                throw new RuntimeException("switch defaultl case");
+        }
+    }
+
+}
+
+/**
+ * Now let's have an interface-related solution.
+ * Covered with tests.
+ * */
+interface Logic {
+
+    String process();
+}
+
+class A implements Logic {
+
+    public static final String INTERFACE_LOGIC_A = "interface Logic: A";
+    public static final String INTERFACE_LOGIC_C = "interface (lambda) Logic: C";
+
+    @Override
+    public String process() {
+        System.out.println(INTERFACE_LOGIC_A);
+        return INTERFACE_LOGIC_A;
+    }
+}
+
+class B implements Logic {
+
+    public static final String INTERFACE_LOGIC_B = "interface Logic: B";
+
+    @Override
+    public String process() {
+        System.out.println(INTERFACE_LOGIC_B);
+        return INTERFACE_LOGIC_B;
+    }
+}
+
+/**
+ * Main class to handle an interface-related solutions.
+ * */
+class InterfaceApproach {
+
+    public Map<String, Logic> logics;
+
+    public InterfaceApproach(Map<String, Logic> logics) {
+        this.logics = logics;
+    }
+
+    public String interfaceApproachTest(String text) {
+        Logic logic = logics.get(text);
+        String process = logic.process();
+        return process;
+    }
+}
+
+
+
+
